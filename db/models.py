2c887e2f2faa2f77f73f613261134e810cd1dea1
Mateusz Mroz <mrozu@student.agh.edu.pl>
2025-12-02 15:04:32 +0100

    Solution for related_names in ForeignKey fields

--- db/models.py
+++ db/models.py
@@ -12,7 +12,7 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
 
     def __str__(self):
         return self.name
@@ -30,8 +30,8 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=False, max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="players")
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True, related_name="players_2")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
